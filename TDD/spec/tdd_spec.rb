43e0eb7394a58a853e677dceee06dec3ad3ef8b5
App Academy Student <student@appacademy.io>
2018-02-21 12:01:00 -0800

    Rename CSS project, add tdd and tdd_spec

---
+++ TDD/spec/tdd_spec.rb
@@ -0,0 +1,13 @@
+require "rspec"
+require "tdd"
+
+RSpec.describe "#my_uniq" do
+  it "return empty array when passed empty array"
+
+  it "does not modify already unique array"
+
+  it "removes non-unique elements"
+
+  it "removes all non-unique elements"
+  
+end
