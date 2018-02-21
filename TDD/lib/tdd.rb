7d7934fae061b5966b8f6deec0cf4b469d402313
App Academy Student <student@appacademy.io>
2018-02-21 13:39:27 -0800

    Implement two_sum and specs for two_sum

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -2,8 +2,25 @@ class Array
   def my_uniq
     uniq_hash = Hash.new(0)
 
-    each { |el| uniq_hash[el] += 1}
+    each { |el| uniq_hash[el] += 1 }
 
     uniq_hash.keys
+  end
+
+  def two_sum
+    output = []
+
+    each_index do |idx|
+      next if idx == length 
+      idx2 = idx + 1
+
+      while idx2 < length
+        if self[idx] + self[idx2] == 0 && self[idx] != 0
+          output << [idx, idx2]
+        end
+        idx2 += 1
+      end
+    end
+    output
   end
 end
--- TDD/spec/tdd_spec.rb
+++ TDD/spec/tdd_spec.rb
@@ -19,3 +19,23 @@ RSpec.describe "#my_uniq" do
   end
 
 end
+
+RSpec.describe "#two_sum" do
+  it "return empty array when passed empty array" do
+    expect([].two_sum).to eq([])
+  end
+
+  it "return empty array when no pairs" do
+    expect([1, 2, 3].two_sum).to eq([])
+  end
+
+  it "returns pairs when present" do
+    expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+  end
+
+  it "doesn't return 0 pairs" do
+    expect([0, 0, 0].two_sum).to eq([])
+  end
+end
+
+#expect().to eq([])
